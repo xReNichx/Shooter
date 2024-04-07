f1e31094426a9c774a5b53b29cd9a406a2db78a6
ReNiсh(Є.С) <142724754+xReNichx@users.noreply.github.com>
2024-04-07 15:18:21 +0300

    Update shooter_game.py
    Добавив пулі і вони летять

--- shooter_game.py
+++ shooter_game.py
@@ -2,24 +2,34 @@
 
 from typing import Any
 from pygame import *
+
 from random import randint
+
+
 # фонова музика
 mixer.init()
 mixer.music.load('space.ogg')
 mixer.music.play()
 fire_sound = mixer.Sound('fire.ogg')
 
+
 font.init()
-font1 = font.Font(None, 36)
-
-
+font1 = font.Font(None,36)
+#win = font1.render('1', (255,255,255))
+#lose = font1.render('2', (255,255,255))
 # нам потрібні такі картинки:
 img_back = "galaxy.jpg"  # фон гри
 img_hero = "rocket.png"  # герой
-img_enemy = "ufo.png"  # ворог
+img_enemy = "ufo.png"
+img_bullet = "bullet.png"
 
 score = 0
 lost = 0
+max_lost = 3
+
+
+
+
 
 
 
@@ -62,17 +72,23 @@ class Player(GameSprite):
 
     # метод "постріл" (використовуємо місце гравця, щоб створити там кулю)
     def fire(self):
-        pass
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
 
 class Enemy(GameSprite):
     def update(self):
         self.rect.y += self.speed
         global lost
         if self.rect.y > win_height:
-            self.rect.x = randint(80, win_width-80)
+            self.rect.x = randint(80,win_width-80)
             self.rect.y = 0
-            lost = lost + 1
+            lost += 1
 
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
 
 
 
@@ -87,9 +103,13 @@ background = transform.scale(image.load(img_back), (win_width, win_height))
 # створюємо спрайти
 ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
 monsters = sprite.Group()
-for i in range(1, 6):
-    monster = Enemy(img_enemy, randint(80, win_width), -40, 80, 50, randint(1, 5))
+
+for i in range(1,6):
+    monster = Enemy(img_enemy, randint(80,win_width-80), -40, 80,50,randint(1,5))
     monsters.add(monster)
+
+bullets = sprite.Group()
+
 # змінна "гра закінчилася": як тільки вона стає True, в основному циклі перестають працювати спрайти
 finish = False
 
@@ -103,27 +123,29 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
 
 
     if not finish:
         # оновлюємо фон
         window.blit(background, (0, 0))
-        text = font1.render('Рахунок:' + str(score), 1, (255, 255, 255))
-        window.blit(text, (10, 50))
+        text = font1.render('рахунок' + str(score), 1, (255,255,255))
+        window.blit(text, (10,50))
 
-        text_lost = font1.render('Пропущено:' + str(lost), 1, (255, 255, 255))
-        window.blit(text_lost, (10, 90))
-
-
+        text_lost = font1.render('Пропущено' + str(lost), 1, (255,255,255))
+        window.blit(text_lost, (10,90))
         # рухи спрайтів
         ship.update()
         monsters.update()
-
+        bullets.update()
 
         # оновлюємо їх у новому місці при кожній ітерації циклу
         ship.reset()
         monsters.draw(window)
-
+        bullets.draw(window)
 
         display.update()
     # цикл спрацьовує кожні 0.05 секунд
